33437f179fb9ecbe2f8ba481653a02ea1a6e7824
OniOni <mathieu.c.sabourin@gmail.com>
2012-06-22 15:17:22 -0700

    Initial commit.

---
+++ example/example.js
@@ -0,0 +1,39 @@
+var webdriver = require('../lib/main')
+  , assert = require('assert');
+
+var parallizer = webdriver.parallizer();
+
+parallizer.run([{ 
+    browserName:'chrome',
+    tags: ["examples"],
+    name: "This is an example test",
+},{
+    browserName:'firefox',
+    tags: ["examples"],
+    name: "This is an example test",
+}], function(browser, desired) {
+
+    browser.on('status', function(info){
+	console.log('\x1b[36m%s\x1b[0m', info);
+    });
+    
+    browser.on('command', function(meth, path){
+	console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
+    });
+    
+    browser.init(desired, function() {
+	browser.get("http://saucelabs.com/test/guinea-pig", function() {
+	    browser.title(function(err, title) {
+		assert.ok(~title.indexOf('I am a page title - Sauce Labs'), 'Wrong title!');
+		browser.elementById('submit', function(err, el) {
+		    browser.clickElement(el, function() {
+			browser.eval("window.location.href", function(err, title) {
+			    assert.ok(~title.indexOf('#'), 'Wrong title!');
+  			    browser.quit()
+			})
+		    })
+		})
+	    })
+	})
+    })
+});
---
+++ example/ondemand.example.js
@@ -0,0 +1,45 @@
+var webdriver = require('../lib/main')
+  , assert = require('assert');
+
+// CONFIGURE SAUCE CREDENTIALS HERE
+var username = "msabourin",
+accessKey = "b9986d37-44b1-42a1-92a7-ef2882bfd502";
+
+var parallizer = webdriver.parallizer({
+    host: "ondemand.saucelabs.com", 
+    port: 80, 
+    username: username,
+    accessKey: accessKey
+});
+
+parallizer.run([
+    {browserName:'chrome', tags: ["examples"], name: "wd parallel async 1/4"},
+    {browserName:'firefox', tags: ["examples"], name: "wd parallel async 2/4"},
+    {browserName:'firefox', tags: ["examples"], name: "wd parallel async 3/4", platform: "LINUX"},
+    {browserName:'firefox', tags: ["examples"], name: "wd parallel async 4/4", platform: "LINUX"}
+], function(browser, desired) {
+
+    browser.on('status', function(info){
+	console.log('\x1b[36m%s\x1b[0m', info);
+    });
+    
+    browser.on('command', function(meth, path){
+	console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
+    });
+    
+    browser.init(desired, function() {
+	browser.get("http://saucelabs.com/test/guinea-pig", function() {
+	    browser.title(function(err, title) {
+		assert.ok(~title.indexOf('I am a page title - Sauce Labs'), 'Wrong title!');
+		browser.elementById('submit', function(err, el) {
+		    browser.clickElement(el, function() {
+			browser.eval("window.location.href", function(err, title) {
+			    assert.ok(~title.indexOf('#'), 'Wrong title!');
+  			    browser.quit()
+			})
+		    })
+		})
+	    })
+	})
+    })
+});
